8fe43c3f1ccc57910aa8e1ec0b7b54b4e0e75d8e
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 22:44:27 +0800

    Add the instruction and pseudocode

---
+++ checks_first_and_last_number_of_list_if_same.py
@@ -0,0 +1,13 @@
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# pseudocode
+
+# define a function that takes a list as an argument
+
+# check if the first and last number of the list are the same
+
+# if they are the same return True
+
+# if they are not the same return False
+
+# write the given list
