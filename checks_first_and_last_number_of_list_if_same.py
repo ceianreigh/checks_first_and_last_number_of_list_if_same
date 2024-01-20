a6cb87af20360046de7975e0a59e19d6beac9acc
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 22:57:05 +0800

    Update same_first_last function to return a string indicating whether the first and last numbers of the list are the same.

--- checks_first_and_last_number_of_list_if_same.py
+++ checks_first_and_last_number_of_list_if_same.py
@@ -10,15 +10,15 @@ def same_first_last(numbers):
     # check if the first and last number of the list are the same
     if numbers[0] == numbers[-1]:
         # if they are the same return True
-        return True
+        return "the same. True."
     # if they are not the same return False
     else:
-        return False
+        return "not the same. False."
 
 
 # write the given list
 numbers_x = [10, 20, 30, 40, 10]
-print("The result is", same_first_last(numbers_x))
+print("The first and last number of the list are", same_first_last(numbers_x))
 
 numbers_y = [75, 65, 35, 75, 30]
-print("The result is", same_first_last(numbers_y))
+print("The first and last number of the list are", same_first_last(numbers_y))
